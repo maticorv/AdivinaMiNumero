0bfcf02ac9a9020a00c269ed68002d51a3d93be9
Unknown <matias_corvalan@outlook.com>
2018-04-27 15:01:57 -0300

    Pruebas Unitarias creadas
    Se crearoon las pruebas unitarias, falta ponerlas a punto

---
+++ test/adivinaminumero/AdivinaMiNumeroTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adivinaminumero;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author matias
+ */
+public class AdivinaMiNumeroTest {
+    
+    public AdivinaMiNumeroTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of jugar method, of class AdivinaMiNumero.
+     */
+    @Test
+    public void testJugar() {
+        System.out.println("jugar");
+        AdivinaMiNumero instance = new AdivinaMiNumero();
+        instance.jugar();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
