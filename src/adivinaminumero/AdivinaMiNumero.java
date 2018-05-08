65b3e1213c6a09d4ba6b180c9ae391269ee2ad25
Unknown <matias_corvalan@outlook.com>
2018-05-08 11:16:37 -0300

    Refactorizacion de codigo
    faltan corregir las pruebas unitarias para este codigo

--- src/adivinaminumero/AdivinaMiNumero.java
+++ src/adivinaminumero/AdivinaMiNumero.java
@@ -3,42 +3,49 @@ import java.util.Scanner;
 
 public class AdivinaMiNumero {
     public final int numeroDesconocido;
-
+    int i;
+    String j;
 
     public AdivinaMiNumero() {
-        this.numeroDesconocido = (int) (Math.random() * 99) + 1;;
+        this.numeroDesconocido = (int) (Math.random() * 100);
     }
-    public void jugar(){
+    public boolean jugar(){
         mostrarIntroduccion();
-        jugada();
-        
+        do{
+            i = solicitarNumero();
+            j=jugada(i);
+            System.out.println(j);
+        }while(!(j.equals("Adivinaste")));
+        return true;
     }
     
     public boolean mostrarIntroduccion(){
         System.out.println("Estoy pensando un numero entre 0 y 100");
         System.out.println("Podes adivinarlo??");
+        System.out.println("mi numero secreto es "+numeroDesconocido);
         return true;
-//        System.out.println("mi numero secreto es"+numeroDesconocido);
     }
     
-    private void jugada(){
+    public int solicitarNumero(){
         System.out.println("Ingresa el numero:");
         try{
             Scanner scan = new Scanner(System.in);
-            int i = scan.nextInt();
-            if(numeroDesconocido==i){
-                System.out.println("Adivinaste");
-            }else{
-                if(numeroDesconocido<i){
-                    System.out.println("Mi numero es mas chico");
-                    jugada();
-                }else{
-                    System.out.println("Mi numero es mas grande");
-                    jugada();
-                }
-            }
+            this.i = scan.nextInt();
         }catch(Exception e){
-            jugada();
+            solicitarNumero();
+        }
+        return i;
+    }
+    
+    private String jugada(int i){
+        if(numeroDesconocido==i){
+            return("Adivinaste");
+        }else{
+            if(numeroDesconocido<i){
+                return("Mi numero es mas chico");
+            }else{
+                return("Mi numero es mas grande");
+            }
         }
     }
 }
